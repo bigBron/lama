c917d9895d00fb1b18b06db3a48ee8328b51255c
NOBODY <NUll@NOBODY.com>
2022-10-25 15:14:13 +0800

    init

---
+++ recover.go
@@ -0,0 +1,58 @@
+package lama
+
+import (
+	"github.com/gookit/validate"
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/context"
+	"time"
+)
+
+type Recover struct {
+	debug bool
+}
+
+func (s *Recover) Init() error {
+	s.debug = NewConf().GetBool("app.debug")
+	return nil
+}
+
+func (s *Recover) UseGlobal() iris.Handler {
+	return func(ctx *context.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				if ctx.IsStopped() { // handled by other middleware.
+					return
+				}
+
+				var code int
+				var msg string
+				ret := map[string]any{
+					"state": false,
+					"time":  time.Now().Unix(),
+				}
+
+				switch e := err.(type) {
+				case string:
+					code = 400
+					msg = e
+
+				case validate.Errors:
+					code = 400
+					msg = e.One()
+
+				default:
+					code = 500
+					msg = "Server internal error"
+					if s.debug {
+						// todo 优化
+						msg = err.(error).Error()
+					}
+				}
+
+				ret["msg"] = msg
+				ctx.StopWithJSON(code, ret)
+			}
+		}()
+		ctx.Next()
+	}
+}
